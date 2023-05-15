c588ad7cfb0d9832d56810a46d426043b50b1a82
HollyT85 <h.e.turner@gmail.com>
2023-05-15 21:37:02 +0000

    fixed broken code - misspelling

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from comments.models import Comment
+from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -15,11 +16,11 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
-            'title', 'content',
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
         ]
 
 
-class CommentDetailSerializer(serializers.ModelSerializer):
+class CommentDetailSerializer(CommentSerializer):
+
     post = serializers.ReadOnlyField(source='post.id')
