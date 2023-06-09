1216d4207aa767666e7ed0f372c6c750ebd9e5e5
HollyT85 <h.e.turner@gmail.com>
2023-06-09 18:16:43 +0000

    .

--- api/settings.py
+++ api/settings.py
@@ -61,7 +61,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'), '8000-hollyt85-api-s17ga60qnwc.ws-eu99.gitpod.io']
 
